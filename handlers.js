d722704b2b3081e7d25193696de07a36b07ba614
James Linnegar <jlinnega@thoughtworks.com>
2018-04-04 09:31:45 +0100

    Answering the phone

--- handlers.js
+++ handlers.js
@@ -4,32 +4,50 @@ var robot = new Robot();
 var handlers = {
     // register custom intent handlers
     "LaunchRequest": function () {
-      this.emit(':ask',
-        "Welcome, what postit would you like?");
+      robot.wake()
+      .then(()=> {
+        this.emit(':ask', "Welcome, what would you like to do?", "Can help?");
+      });
     },
 
     "RotateArm": function(){
-      var rotation = 90;
-      robot.rotateArmTo(rotation)
-      .then(()=> {
-        this.emit(':ask',
-        `Rotating arm to ${rotation} degrees`);
-      });
+      var rotation = parseInt(this.event.request.intent.slots.angle.value);
+      if(rotation <= 180){
+        robot.rotateArmTo(rotation)
+        .then(()=> {
+          this.emit(':ask', `Rotating arm to ${rotation} degrees`, "Can help?");
+        });
+      } else {
+        this.emit(':ask', `I'm sorry ${rotation} degrees is beyond my ability`, "Can help?");
+      }
     },
 
     "RotateHand": function(){
-      var rotation = 90;
-      robot.rotateHandTo(rotation)
-      .then(()=> {
+      var rotation = parseInt(this.event.request.intent.slots.angle.value);
+      if(rotation <= 180){
+        robot.rotateHandTo(rotation)
+        .then(()=> {
+          this.emit(':ask',
+          `Rotating hand to ${rotation} degrees`, "Can help?");
+        });
+      } else {
         this.emit(':ask',
-        `Rotating hand to ${rotation} degrees`);
-      });
+        `I'm sorry, ${rotation} degrees is beyond my ability`, "Can help?");
+      }
     },
 
     "PickUp": function(){
       robot.pickUp()
       .then(()=> {
         this.emit(':ask',
+        `Picking up`, "Can help?");
+      });
+    },
+
+    "Answerphone": function(){
+      robot.phone()
+      .then(()=> {
+        this.emit(':tell',
         `Picking up`);
       });
     },
@@ -38,7 +56,7 @@ var handlers = {
       robot.putDown()
       .then(()=> {
         this.emit(':ask',
-        'dropping');
+        'dropping', "Can help?");
       });
     },
 
@@ -46,7 +64,7 @@ var handlers = {
       robot.raiseArm()
       .then(()=> {
         this.emit(':ask',
-        `Raising the arm`);
+        `Raising the arm`, "Can help?");
       });
     },
 
@@ -54,27 +72,29 @@ var handlers = {
       robot.lowerArm()
       .then(()=> {
         this.emit(':ask',
-        `Lowering the arm`);
+        `Lowering the arm`, "Can help?");
       });
     },
 
-    "Card":  function () {
-      this.emit(':tell',
-        "Picking up a yellow card");
-    },
-
     "AMAZON.HelpIntent": function (intent, session, response) {
-      this.emit(':tell',
-        "You can ask for tram times to and from stations, set your home station and ask for next trams.",
-        "For example, You can ask 'when is the next tram from Chorlton to Eccles'");
+      this.emit(':ask',
+        "You can ask me to raise the arm, rotate to an angle, pick up or drop.",
+        "If you don't say anything I'll just wait.");
     },
 
     "AMAZON.CancelIntent": function(intent, session, response){
+      robot.sleep();
       this.emit(':tell', "goodbye");
     },
 
     "AMAZON.StopIntent": function(intent, session, response){
+      robot.sleep();
       this.emit(':tell', "goodbye");
+    },
+
+    'SessionEndedRequest': function () {
+        console.log('session ended!');
+        robot.sleep();
     },
 
     'Unhandled': function() {
